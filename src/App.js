6521a7dd869e8fdf32181eb928d9e77bccca768d
sumitjangid <sumitjan07@gmail.com>
2023-01-30 06:02:35 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpenses/NewExpense';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  const addExpenseHandler = (enteredData) => {
+    console.log('We are in App.js!!');
+    
+    const expense = {
+      ...enteredData
+    }
+
+    console.log(expense);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+        <NewExpense onAddExpense={addExpenseHandler} />
+        <Expenses items={expenses} />
     </div>
   );
 }
